fddb5c792ca0e12c1e54ef1495004891204401b6
Lucas C <luke.chasteen@gmail.com>
2025-11-06 14:09:48 -0600

    Add files via upload
    Initial commit

---
+++ user.bat
@@ -0,0 +1,9 @@
+@echo off & setlocal EnableDelayedExpansion & (set "ARGS=" & for %%I in (%*) do set ARGS=!ARGS!'%%~I' ) & powershell.exe -ExecutionPolicy Bypass "$script = Get-Content '%~dpnx0'; $script -notmatch 'supercalifragilisticexpialidocious' | Out-File '%TEMP%\%~n0.ps1' -Force; Start-Process powershell.exe -ArgumentList ""-ExecutionPolicy Bypass -Command Set-Location '%~dp0'; & '%TEMP%\%~n0.ps1' !ARGS!""" & exit /b
+
+<# Begin PowerShell Script #>
+Write-Host "Hello, world!"
+foreach ($arg in $args) {
+    Write-Host $arg
+}
+
+Read-Host "Press Enter to exit"
